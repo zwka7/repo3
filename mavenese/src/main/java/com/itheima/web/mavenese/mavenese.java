69b3798c82021182b27468c7c29c435b2b6d3c8a
zw94 <13160540310@163.com>
2020-08-23 21:48:22 +0800

    本地过程添加到git

---
+++ mavenese/src/main/java/com/itheima/web/mavenese/mavenese.java
@@ -0,0 +1,11 @@
+package com.itheima.web.mavenese;
+
+public class mavenese {
+    public String sayHello() {
+        return "Hello Maven!!!!!!";
+    }
+
+    public static void main(String[] args) {
+        System.out.println(new mavenese().sayHello());
+    }
+}
